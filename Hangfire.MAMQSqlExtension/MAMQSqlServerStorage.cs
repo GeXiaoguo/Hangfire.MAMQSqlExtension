5c0725ae004bb9d563f0de15a190f4a349257e4e
Sergey Odinokov <odinserj@gmail.com>
2023-05-04 16:41:02 +0700

    Prefer composition over inheritance for the storage class

--- Hangfire.MAMQSqlExtension/MAMQSqlServerStorage.cs
+++ Hangfire.MAMQSqlExtension/MAMQSqlServerStorage.cs
@@ -1,22 +1,49 @@
 ﻿
 namespace Hangfire.MAMQSqlExtension
 {
+    using Hangfire.Logging;
+    using Hangfire.Server;
+    using Hangfire.States;
     using Hangfire.Storage;
     using Hangfire.SqlServer;
     using System.Collections.Generic;
 
-    public class MAMQSqlServerStorage : SqlServerStorage
+    public class MAMQSqlServerStorage : JobStorage
     {
-        private IEnumerable<string>? _queues;
+        private readonly IEnumerable<string>? _queues;
+        private readonly SqlServerStorage _inner;
 
-        public MAMQSqlServerStorage(string nameOrConnectionString, SqlServerStorageOptions options, IEnumerable<string>? queues) : base(nameOrConnectionString, options)
+        public MAMQSqlServerStorage(string nameOrConnectionString, SqlServerStorageOptions options, IEnumerable<string>? queues)
         {
+            _inner = new SqlServerStorage(nameOrConnectionString, options);
             _queues = queues;
         }
 
+        public override bool LinearizableReads => _inner.LinearizableReads;
+
         public override IStorageConnection GetConnection()
         {
-            return new MAMQSqlServerConnection(this, _queues);
+            return new MAMQSqlServerConnection(_inner, _queues);
+        }
+
+        public override IMonitoringApi GetMonitoringApi()
+        {
+            return _inner.GetMonitoringApi();
+        }
+
+        public override IEnumerable<IServerComponent> GetComponents()
+        {
+            return _inner.GetComponents();
+        }
+
+        public override IEnumerable<IStateHandler> GetStateHandlers()
+        {
+            return _inner.GetStateHandlers();
+        }
+
+        public override void WriteOptionsToLog(ILog logger)
+        {
+            _inner.WriteOptionsToLog(logger);
         }
     }
 }
